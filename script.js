cf99affae7ab600acf6dcd8c69fc06d2066cfb2b
Katarzyna Grabowska <kati.grab@gmail.com>
2020-03-09 19:54:43 +0100

    Add new files

---
+++ script.js
@@ -0,0 +1,74 @@
+//import React from "react";
+//import ReactDOM from "react-dom";
+
+var movies = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'Film o czarodzieju',
+        img: 'https://cdn-3.cdp.pl/media/catalog/product/cache/1/image/600x/17f82f742ffe127f42dca9de82fb58b1/1/3/13916_0.jpg'
+    },
+    {
+        id: 2,
+        title: 'Król lew',
+        desc: 'Film o królu sawanny',
+        img: 'https://fwcdn.pl/fpo/63/16/776316/7892306.3.jpg'
+    },
+    {
+        id: 3,
+        title: 'Avatar',
+        desc: 'Film fantasy',
+        img: 'https://static.posters.cz/image/750/plakaty/avatar-limited-ed-couple-i7199.jpg'
+    },
+    {
+        id: 4,
+        title: 'Włladca Pierścieni',
+        desc: 'Film o podróży Bilbo Bagginsa',
+        img: 'https://mb.web.sapo.io/7622ca43ac86cc2d41b890d3488599cc2e80e26f.jpg'
+    }
+];
+
+var Movie = React.createClass({
+    render: function() {
+        return React.createElement("li", { key: this.props.movie.id },
+            React.createElement(MovieTitle, { movie: this.props.movie }),
+            React.createElement(MovieDescription, { movie: this.props.movie.desc }),
+            React.createElement("img", { src: this.props.movie.img })
+        );
+    },
+    propTypes: {
+        image: React.PropTypes.object.isRequired,
+        des: React.PropTypes.object.isRequired,
+        title: React.PropTypes.object.isRequired
+    }
+});
+
+var MovieTitle = React.createClass({
+    render: function() {
+        return React.createElement("h2", {}, this.props.movie.title);
+    },
+    propTypes: {
+        title: React.PropTypes.object.isRequired
+    }
+});
+
+/*var MovieDescription = React.createClass({
+    render: function() {
+        return React.createElement("p", {}, this.props.movie.desc);
+    },
+    propTypes: {
+        des: React.PropTypes.object.isRequired
+    }
+});
+*/
+
+var moviesElements = movies.map(function(movie) {
+    return React.createElement(Movie, { movie: movie });
+});
+
+var element = React.createElement("div", {},
+    React.createElement("h1", {}, "Lista filmów"),
+    React.createElement("ul", {}, moviesElements)
+);
+
+ReactDOM.render(element, document.getElementById("app"));
